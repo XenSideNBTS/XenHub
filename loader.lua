d2121bb41788ba669c22ecc3200675d4e5f122e7
XenSide Main <112946743+XenSideNBTS@users.noreply.github.com>
2022-11-11 22:31:23 +0300

    Delete loader.lua

--- loader.lua
+++
@@ -1,7 +0,0 @@
-_G.KEYST = "13E8D8A112EE49FB"
-_G.KEYUSER = ""
-      if _G.KEYUSER == _G.KEY then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NooVster/BB-MB/main/Source"))()
-end
-
-
